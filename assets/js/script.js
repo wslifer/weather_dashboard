3e6f822bb012d769859ab73c0b47f7b8938b2ff7
Wesley Slifer <wesleyslifer@ymail.com>
2020-04-19 14:35:42 -0500

    readding functions after research

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,71 @@
-var cityList = $("#cityList");
-var cities = [];
-
 // setting functions up
+var city = "";
+var cities = "";
+var citiesDiv = $("#searched_cities_container");
 
-// AJAX Calls to API
+init();
+listClick();
+searchClick();
+
+function init() {
+  let saved_cities = JSON.parse(localStorage.getItem("cities"));
+
+  if (saved_cities !== null) {
+    cities = saved_cities;
+  }
+
+  renderButtons();
+}
+
+function storeCities() {
+  localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+function renderCities() {
+  citiesDiv.innerHTML = "";
+  if (cities == null) {
+    return;
+  }
+  let unique_cities = [...new Set(cities)];
+  for (let i = 0; i < unique_cities.length; i++) {
+    let cityName = unique_cities[i];
+
+    let buttonEl = document.createElement("button");
+    buttonEl.textContent = cityName;
+    buttonEl.setAttribute("class", "listbtn");
+
+    citiesDiv.appendChild(buttonEl);
+    listClicker();
+  }
+}
+function listClick() {
+  $(".listbtn").click(function (event) {
+    event.preventDefault();
+    city = $(this).text().trim();
+    APIcalls();
+  });
+}
+function searchClicker() {
+  $("#searchbtn").click(function (event) {
+    event.preventDefault();
+    city = $(this).prev().val().trim();
+
+    cities.push(city);
+
+    if (cities.length > 8) {
+      cities.shift();
+    }
+
+    if (city == "") {
+      return;
+    }
+    APIcalls();
+    storeCities();
+    renderButtons();
+  });
+}
+
+// AJAX Calls to API (One Call)
 var queryURL =
   "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=" +
   apiKEY;
