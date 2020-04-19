3da1fdabe1c30d459bd4747ba1b8c18cc3310a66
Wesley Slifer <wesleyslifer@ymail.com>
2020-04-19 09:56:11 -0500

    adding readme for later. set vars for URL and API Key

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var cityList = $("#cityList");
+var cities = [];
+
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={apiKEY}";
+var apiKEY = "dd95bf9ea6613905902595b35a625a38";
